58fd48b24ed44e4e8aec09029fe8ec01ffa774e2
Yujie Zhou <309056101@qq.com>
2019-01-01 10:26:26 +0800

    309056101@qq.com

--- src/main/resources/static/app/measure.js
+++ src/main/resources/static/app/measure.js
@@ -1,8 +1,3 @@
-var IP = '47.106.130.222';
-var measureUrl = 'http://' + IP + '/master/procedure/query';
-var finishUrl = 'http://' + IP + '/master/procedure/finish';
-var restartUrl = 'http://' + IP + '/master/procedure/restart';
-var exportUrl = 'http://' + IP + '/master/measure/export';
 
 function updateMeasure(callback) {
     $.ajax({
--- src/main/resources/static/app/measureDetail.js
+++ src/main/resources/static/app/measureDetail.js
@@ -1,9 +1,3 @@
-var IP = '47.106.130.222';
-var measureUrl = 'http://' + IP + '/master/procedure/query';
-var finishUrl = 'http://' + IP + '/master/procedure/finish';
-var restartUrl = 'http://' + IP + '/master/procedure/restart';
-var exportUrl = 'http://' + IP + '/master/measure/export';
-
 
 $(document).ready(function () {
     var loc = location.href;
@@ -11,6 +5,6 @@ $(document).ready(function () {
     if (!procedure) {
         return;
     }
-    
+
     $(".procedureName").text(procedure);
 });
